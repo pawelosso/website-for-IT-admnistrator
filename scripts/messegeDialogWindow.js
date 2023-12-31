e493817c1214cdd1200832ec84a2f56dc73d0767
pawel0699@o2.pl <pawel0699@o2.pl>
2023-12-31 14:23:45 +0100

    changes in message Animation

--- scripts/messegeDialogWindow.js
+++ scripts/messegeDialogWindow.js
@@ -1,5 +1,50 @@
 const buttonToOpen = document.querySelector('.messageButton');
 
+const hideForm = () => {
+  document.querySelector('.messageInfo').classList.add('hide');
+  document.querySelector('.messageInfo-container').classList.add('hide');
+  document.querySelector('.messageInfo-container').addEventListener('transitionend', ()=>{
+    document.querySelectorAll('.blurWhenMessegeActive').forEach(element => element.classList.remove('blur'))
+    document.querySelector('.messageInfo-container').remove();
+    buttonToOpen.classList.remove('hide');
+  })
+}
+
+const showForm = () => {
+  const messageBody = `
+    <div class="messageInfo hide">
+      <div class="closeButton">
+        <img src="/images/close.png" alt="closeButton">
+      </div>
+      <p>lukasz.aleksandrowicz@allcon.pl</p>
+      <div class="images">
+        <a href="#">
+          <img src="/images/linkedin.png" alt="linkedin">
+        </a>
+        <a href="#">
+          <img src="/images/github.png" alt="github">
+        </a>
+      </div>
+    </div>`;
+
+  buttonToOpen.classList.add('hide');
+  let isTransitionEndHandled = false;
+
+  buttonToOpen.addEventListener('transitionend', event => {
+    if (!isTransitionEndHandled && event.propertyName === 'opacity') {
+      const createWindow = document.createElement('div');
+      document.querySelector('body').prepend(createWindow)
+      createWindow.classList.add('messageInfo-container');
+      createWindow.innerHTML = messageBody;
+    
+      setTimeout(()=>document.querySelector('.messageInfo').classList.remove('hide'));
+      document.querySelector('.messageInfo .closeButton').addEventListener('click', ()=> hideForm())
+      
+      isTransitionEndHandled = true;
+    }
+  })
+}
+
 
 const blurScreen = () => {
   document.querySelectorAll('.blurWhenMessegeActive').forEach(element => {
@@ -7,8 +52,10 @@ const blurScreen = () => {
   })
 }
 
+
 buttonToOpen.addEventListener('click',()=> {
   blurScreen();
+  showForm();
 })
 
 
