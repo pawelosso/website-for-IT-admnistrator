3a32a54ce9d5d6166a3defce87bfd2d29eab8967
pawel0699@o2.pl <pawel0699@o2.pl>
2024-01-07 15:33:10 +0100

    changes in scripts and css

---
+++ scripts/scrollUp.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior:"smooth",
+    });
+    console.log('Page loaded, scrolled to the top');
+});
+
